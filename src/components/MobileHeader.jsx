6757982d9bab4bc130e2c3ae95bfcd939d9b6f89
gavinhousley <gavinhousley@gmail.com>
2026-08-28 17:12:16 +0100

    adjust desktop view to widen columns and restrict modal width, and increase font sizes overall for modal on desktop and mobile

--- src/components/MobileHeader.jsx
+++ src/components/MobileHeader.jsx
@@ -4,6 +4,10 @@ export default function MobileHeader() {
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>100 BOOKS</h1>
+      <p className={styles.subtitle}>
+        100 greatest books of all time as voted by the Norwegian Book Club in
+        2002
+      </p>
     </header>
   );
 }
